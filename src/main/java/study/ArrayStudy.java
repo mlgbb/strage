0e128abc910943ae83fcaead5924254fd4b2d13c
guoyf <guoyf@cadg.cn>
2019-11-25 20:39:08 +0800

    数组学习

---
+++ src/main/java/study/ArrayStudy.java
@@ -0,0 +1,10 @@
+package study;
+
+public class ArrayStudy {
+    public static void main(String[] args) {
+        // 数组 线型的序列可以快速的访问其他元素，数组在创建后容量是不变的也就是说固定了一个length
+        // 并且在运行的时候会用这个length去做越界检查
+        // 初始化一个数组 数据类型 [] 数组名称 = new 数据类型[长度];
+
+    }
+}
