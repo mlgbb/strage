a3d5beac5a9ab5f9903bade92d801eab690c2cf0
zhuqianfeng <zhuqf@cadg.cn>
2019-12-17 19:43:21 +0800

    swagger功能添加

--- src/main/java/com/example/strage/controller/TestController.java
+++ src/main/java/com/example/strage/controller/TestController.java
@@ -1,4 +1,27 @@
 package com.example.strage.controller;
 
+import com.example.strage.io.IOUtilsDemo;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@Api(tags = {"测试接口"})
+@RequestMapping("/test")
 public class TestController {
+
+    @Autowired
+    IOUtilsDemo ioUtilsDemo;
+
+    @GetMapping("/download")
+    public void downloadFile(HttpServletResponse response){
+        ioUtilsDemo.downloadFile(response);
+
+    }
 }
